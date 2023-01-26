912c1021208532d499de98c41ad83ec8a6f31596
Александр Биктеев <bikteev@mail.ru>
2023-01-26 21:21:49 +0600

    1. Add an error wrapper structure 2. Create an error view 3. Report errors 4. Simulate data corruption

---
+++ Scrumdinger/ErrorView.swift
@@ -0,0 +1,55 @@
+//
+//  ErrorView.swift
+//  Scrumdinger
+//
+//  Created by albik on 26.01.2023.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let errorWraper: ErrorWrapper
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("An error has occurred!")
+                    .font(.title)
+                    .padding(.bottom)
+                Text(errorWraper.error.localizedDescription)
+                    .font(.headline)
+                Text(errorWraper.guidance)
+                    .font(.caption)
+                    .padding(.top)
+                Spacer()
+            }
+            .padding()
+            .background(.ultraThinMaterial)
+            .cornerRadius(16)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button("Dismiss") {
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired,
+                     guidance: "You can safely ignore this error.")
+    }
+    
+    static var previews: some View {
+        ErrorView(errorWraper: wrapper)
+    }
+}
